38ef6f1c9ed4b43239e850fa12e6e322ea31649c
Isomorphic03 <vishalcodes03@gmail.com>
2024-11-04 12:45:25 +0530

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+/*The extended Euclidean algorithm is an algorithm that, given an integer pair 
+(a,b) as input, finds an integer pair 
+(x,y) such that 
+ax+by=±gcd(a,b) in 
+O(logmin(∣a∣,∣b∣))*/
+
+// Sample implementation of extended Euclidean algorithm
+pair<long long, long long> extgcd(long long a, long long b) {
+  if (b == 0) return make_pair(1, 0);
+  long long x, y;
+  tie(y, x) = extgcd(b, a % b);
+  y -= a / b * x;
+  return make_pair(x, y);
+}
+
